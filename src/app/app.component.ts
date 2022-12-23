6f678b10ae4e63bc48b48012d080fbfaec9fdae9
EVL57 <99217323+EVL57@users.noreply.github.com>
2022-12-23 09:26:35 +0000

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  template: '<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>'
+  template: `<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>`
 })
 export class AppComponent{
   pageTitle: string = 'My Product Management';
